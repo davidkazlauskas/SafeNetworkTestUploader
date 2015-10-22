bbe173571f76864f6e9eb0f7b165050f9a064d35
David <david@templatious.org>
2015-10-22 17:44:36 +0300

    compiled

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let the_args : Vec<_> = env::args().collect();
+
+    if the_args.len() != 3 || the_args.len() != 4 {
+        println!("Usage: uploadutil <action: upl/dl> <local file> <remote folder>");
+    }
 }
