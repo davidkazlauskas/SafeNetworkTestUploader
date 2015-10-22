df0f478e0693307b7b278c47f6f405851f45a10d
David <david@templatious.org>
2015-10-22 18:20:10 +0300

    return

--- src/main.rs
+++ src/main.rs
@@ -52,6 +52,7 @@ fn main() {
 
     if the_args.len() != 3 || the_args.len() != 4 {
         println!("Usage: uploadutil <action: upl/dl> <local file> <remote folder>");
+        return;
     }
 
     let login = login();
