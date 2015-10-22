3a59706dd4572de7b62f6deacce5ffa43948e31d
David <david@templatious.org>
2015-10-22 18:07:46 +0300

    login attempt

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,35 @@
 extern crate safe_core;
 
 use std::env;
+use std::io;
+use std::io::BufReader;
+use std::io::prelude::*;
+use std::fs::File;
 use safe_core::client::Client;
 
-fn login() -> Result< Client, safe_core::errors::CoreError > {
-    Client::log_in("moo".to_string(),"goo".to_string(),"goo".to_string())
+fn login() -> Client {
+    let mut keyword = "";
+    let mut pin = "";
+    let mut password = "";
+
+    match env::var("TEST_SAFENETWORK_LOGIN_PATH") {
+        Ok(val) => {
+            let f = match File::open(val) {
+                Ok(file) => {
+                    let reader = BufReader::new(file);
+                    let lines : Vec<_> = reader.lines().collect();
+                },
+                Err(err) => {
+                    panic!("Could not open file");
+                },
+            };
+        },
+        Err(err) => {
+            panic!("No enviroment variable TEST_SAFENETWORK_LOGIN_PATH");
+        }
+    }
+
+    Client::log_in("moo".to_string(),"goo".to_string(),"goo".to_string()).unwrap()
 }
 
 fn main() {
@@ -13,4 +38,6 @@ fn main() {
     if the_args.len() != 3 || the_args.len() != 4 {
         println!("Usage: uploadutil <action: upl/dl> <local file> <remote folder>");
     }
+
+    let login = login();
 }
